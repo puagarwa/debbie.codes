a0cc0a83b38533c81cf12fefc4710ad21a8bab61
Puneet Agarwal <28705989+puagarwa@users.noreply.github.com>
2023-10-25 18:10:18 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,6 +71,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://127.0.0.1:3020',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: process.env.CI,
   },
 });
