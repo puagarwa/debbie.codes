78e88d5414036afc92fe4048c34bf77291ced69e
Puneet Agarwal <28705989+puagarwa@users.noreply.github.com>
2023-10-25 23:44:11 +0530

    Update playwright.service.config.ts

--- playwright.service.config.ts
+++ playwright.service.config.ts
@@ -29,7 +29,7 @@ const os = process.env.PLAYWRIGHT_SERVICE_OS || 'linux';
 
 export default defineConfig(config, {
   // Define more generous timeout for the service operation if necessary.
-   timeout: 60000,
+   timeout: 120000,
    expect: {
      timeout: 10000,
    },
